a09262c1cf89e569c114b04afc3ab8254fc069fc
Vitali Pantsialei <bitvitalby@gmail.com>
2015-04-03 12:16:56 +0300

    add upload file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,13 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using OpenCalaisExample.Calais;
 using OpenCalaisExample.Models;
+using System.IO;
 
 namespace OpenCalaisExample.Controllers
 {
     public class HomeController : Controller
     {
         private string apiKey = "w39fuahdc3s8xrmb63pvxtdg";
-        static string paramsRDF = 
+        static string paramsRDF =
 @"<c:params xmlns:c=""http://s.opencalais.com/1/pred/"" xmlns:rdf=""http://www.w3.org/1999/02/22-rdf-syntax-ns#"">
 <c:processingDirectives c:allowDistribution=""true"" c:allowSearch=""true"" c:contentType=""text/txt"" c:outputFormat=""xml/rdf"">
 </c:processingDirectives><c:externalMetadata></c:externalMetadata>
@@ -24,18 +25,24 @@ namespace OpenCalaisExample.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string inputData)
+        public ActionResult Index(string inputData, HttpPostedFileBase file)
         {
-            string resp = null;
             TransformationModel tm = new TransformationModel();
+            tm.XsltPath = Server.MapPath("~/Content/index.xslt");
+            if (file != null)
+            {
+                using (BinaryReader b = new BinaryReader(file.InputStream))
+                {
+                    byte[] binData = b.ReadBytes(file.ContentLength);
+                    inputData = System.Text.Encoding.UTF8.GetString(binData);
+                }
+            }
             try
             {
                 Calais.calaisSoapClient client = new calaisSoapClient();
-                resp = client.Enlighten(apiKey, inputData, paramsRDF);
-                tm.XmlFile = resp;
-                tm.XsltPath = Server.MapPath("~/Content/index.xslt");
+                tm.XmlFile = client.Enlighten(apiKey, inputData, paramsRDF);
             }
-            catch(Exception)
+            catch (Exception)
             {
                 tm = null;
             }
